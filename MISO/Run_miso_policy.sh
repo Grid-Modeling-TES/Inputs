1abea6a63edaed600259289b388a5f9436bae1ef
samvdjagt <sam.vanderjagt@gmail.com>
2025-07-09 11:58:44 -0700

    Update to new file names

--- MISO/Run_miso_policy.sh
+++ MISO/Run_miso_policy.sh
@@ -7,18 +7,18 @@
 #SBATCH --mem-per-cpu=3G                    # memory per cpu-core
 #SBATCH --time=80:00:00                     # total run time limit (HH:MM:SS)
 
-srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/High_tariff_10_1w_8mmbtu/Run.jl &
-srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/No_tariff_1w_8mmbtu/Run.jl &
-srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/Low_vre_1w_8mmbtu/Run.jl &
-srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/No_co2_cap_1w_8mmbtu/Run.jl &
+srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/High_tariff_10_05w_refgas/Run.jl &
+srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/No_tariff_05w_refgas/Run.jl &
+srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/Low_vre_05w_refgas/Run.jl &
+srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/No_co2_cap_05w_refgas/Run.jl &
 srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/No_co2_cap_reference/Run.jl &
-srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/High_co2_cap_1w_8mmbtu/Run.jl &
-srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/Highest_co2_cap_1w_8mmbtu/Run.jl &
-srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/Highest2_co2_cap_1w_8mmbtu/Run.jl &
+srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/High_co2_cap_05w_refgas/Run.jl &
+srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/Highest_co2_cap_05w_refgas/Run.jl &
+srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/Highest2_co2_cap_05w_refgas/Run.jl &
 srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/High_co2_cap_reference/Run.jl &
 srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/Highest_co2_cap_reference/Run.jl &
 srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/Highest2_co2_cap_reference/Run.jl &
-srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/Qualified_supply_1w_8mmbtu/Run.jl &
+srun -N 1 -n 1 /home/ubuntu/julia-1.6.7/bin/julia --project=. /home/ubuntu/inputs/GenX_inputs/MISO/Scenarios/Qualified_supply_05w_refgas/Run.jl &
 
 wait
 
